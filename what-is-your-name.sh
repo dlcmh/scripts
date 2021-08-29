69154cada40befae3d36e1cb749aeb28911223a3
David Chin <dlcmhd@me.com>
2021-08-29 11:34:41 +0800

    Update what-is-your-name.sh

--- what-is-your-name.sh
+++ what-is-your-name.sh
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/questions/5735666/execute-bash-script-from-url/5735730#5735730
 
 # Run with:
-
+# bash <(curl -s https://raw.githubusercontent.com/dlcmh/scripts/main/what-is-your-name.sh)
 
 echo "Hello, world!"
 read -p "What is your name? " name
