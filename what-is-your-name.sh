54692e8bc6e38bfa2614330a25c6051c076cb9c5
David Chin <dlcmhd@me.com>
2021-08-29 11:33:52 +0800

    Create what-is-your-name.sh

---
+++ what-is-your-name.sh
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/5735666/execute-bash-script-from-url/5735730#5735730
+
+# Run with:
+
+
+echo "Hello, world!"
+read -p "What is your name? " name
+echo "Hello, ${name}!"
